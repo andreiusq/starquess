80abd957c7e31f09df0355adbf18c4f73adc092f
andreiusq <mrandyadv@gmail.com>
2023-02-08 08:27:54 +0200

    register page updated

--- register.php
+++ register.php
@@ -89,7 +89,7 @@ require 'backend/config/db.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="styles/FontAwesome/css/all.css">
-    <link rel="stylesheet" href="styles/register/all.css">
+    <link rel="stylesheet" href="styles/register/register.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.min.css'>
 </head>
