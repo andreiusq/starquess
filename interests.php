bc62fc3a5d4cc7f52975d599ea600a1bd75a6ab1
andreiusq <mrandyadv@gmail.com>
2023-02-15 18:43:52 +0200

    Renamed a file

--- interests.php
+++ interests.php
@@ -21,7 +21,7 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Interests</title>
     <link rel="stylesheet" href="styles/FontAwesome/css/all.css">
     <link rel="stylesheet" href="styles/register/all.css">
     <link rel="stylesheet" href="styles/register/interests.css">
