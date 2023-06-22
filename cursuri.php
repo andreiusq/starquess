01badd2f5a9eba706e0f6c925e7fc5efe0eda012
andreiusq <mrandyadv@gmail.com>
2023-06-22 14:15:56 +0300

    Create cursuri.php

---
+++ cursuri.php
@@ -0,0 +1,38 @@
+<?php
+
+
+define('BASEPATH', true);
+session_start();
+require('backend/config/db.php');
+$is_administrator = 0;
+
+if(!isset($_SESSION["user"])) {
+    header("Location: login.php");
+    exit();
+  }
+
+
+
+
+  ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cursuri</title>
+    <link rel="preload" href="styles/FontAwesome/css/all.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="stylesheet" href="styles/documente/all.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.min.css'>
+</head>
+<body>
+
+<?php include './important/Rightbar.php'; ?>
+    <?php include './important/Sidebar.php'; ?>
+    
+</body>
+</html>
