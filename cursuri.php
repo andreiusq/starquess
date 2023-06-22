9304a2d2bb4d20096fcc057d07cd777456c7e660
andreiusq <mrandyadv@gmail.com>
2023-06-22 14:25:11 +0300

    CSS added to courses

--- cursuri.php
+++ cursuri.php
@@ -25,7 +25,7 @@ if(!isset($_SESSION["user"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cursuri</title>
     <link rel="preload" href="styles/FontAwesome/css/all.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-    <link rel="stylesheet" href="styles/documente/all.css">
+    <link rel="stylesheet" href="styles/cursuri/all.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.min.css'>
 </head>
