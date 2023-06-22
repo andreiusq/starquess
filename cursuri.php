bd4c706f0774a33585f8c8af4fa26540e6352fa8
andreiusq <mrandyadv@gmail.com>
2023-06-22 17:11:35 +0300

    Get all courses from database

--- cursuri.php
+++ cursuri.php
@@ -1,6 +1,4 @@
 <?php
-
-
 define('BASEPATH', true);
 session_start();
 require('backend/config/db.php');
@@ -9,23 +7,26 @@ $is_administrator = 0;
 if(!isset($_SESSION["user"])) {
     header("Location: login.php");
     exit();
-  }
+}
 
-
-
-  // cursuri count
+// cursuri count
 $query = "SELECT COUNT(*) AS row_count FROM courses";
 $stmt = $pdo->query($query);
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 $courses_count = $result['row_count'];
 
+// teste count
 $query = "SELECT COUNT(*) AS row_count FROM tests";
 $stmt = $pdo->query($query);
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 $tests_count = $result['row_count'];
 
 
-  ?>
+// get all courses from the database
+$query = "SELECT id, course_name, course_author, course_link, course_image FROM courses LIMIT 5";
+$stmt = $pdo->query($query);
+$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 
 
 <!DOCTYPE html>
@@ -45,12 +46,9 @@ $tests_count = $result['row_count'];
         <?php include './important/Rightbar.php'; ?>
         <?php include './important/Sidebar.php'; ?>
 
-
-
         <!-- stats -->
 
         <div class="stats">
-
             <div class="all-courses">
                 <div class="rounded-circle">
                     <i class="fa-solid fa-book bookk fa-2xl"></i>
@@ -80,15 +78,11 @@ $tests_count = $result['row_count'];
                     <p class="all-favorite-desc">Cursuri favorite</p>
                 </div>
             </div>
-
         </div>
-
         <!-- end of stats -->
     
-
         <div class="content">
         <!-- cursurile tale -->
-
             <div class="cursuri-box">
                 <div class="cursuri-box-head">
                     <h3 class="cursuri-box-title">Cursurile tale</h3>
@@ -109,33 +103,13 @@ $tests_count = $result['row_count'];
                     <a href="#" class="cursuri-box-link">Vezi toate cursurile</a> <i class="fa-regular fa-arrow-right sagetuta"></i>
                 </div>
                 <div class="cursuri-recomandate-box-content">
-                    <div class="curs">
-                        <img class="curs-image" src="https://via.placeholder.com/144x146" />
-                        <div class="curs-box"></div>
-                        <div class="curs-box-title">Titlu</div>
-                    </div>
-                    <div class="curs">
-                        <img class="curs-image" src="https://via.placeholder.com/144x146" />
-                        <div class="curs-box"></div>
-                        <div class="curs-box-title">Titlu</div>
-                    </div>
-                    <div class="curs">
-                        <img class="curs-image" src="https://via.placeholder.com/144x146" />
-                        <div class="curs-box"></div>
-                        <div class="curs-box-title">Titlu</div>
-                    </div>
-                    <div class="curs">
-                        <img class="curs-image" src="https://via.placeholder.com/144x146" />
-                        <div class="curs-box"></div>
-                        <div class="curs-box-title">Titlu</div>
-                    </div>
-                    <div class="curs">
-                        <img class="curs-image" src="https://via.placeholder.com/144x146" />
-                        <div class="curs-box"></div>
-                        <div class="curs-box-title">Titlu</div>
-                    </div>
-
-                    
+                    <?php foreach ($courses as $course): ?>
+                        <div class="curs">
+                            <img class="curs-image" src="<?php echo $course['course_image']; ?>" />
+                            <div class="curs-box"></div>
+                            <div class="curs-box-title"><?php echo $course['course_name']; ?></div>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
